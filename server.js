182e559b32c38248b7d7060c74a352ca61a20bd9
Armando Berlanga <Jose.berlangam@udem.edu>
2022-10-04 23:31:32 +0200

    k

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const
-  express = require('express'),
-  serveStatic = require('serve-static'),
-  history = require('connect-history-api-fallback'),
-  port = process.env.PORT || 5000
+    express = require('express'),
+    serveStatic = require('serve-static'),
+    history = require('connect-history-api-fallback'),
+    port = process.env.PORT || 5000
 
 const app = express()
 
